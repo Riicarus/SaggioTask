d6abdf7feffa1bd81ea3a39794385213a6c9178c
Riicarus <riicarus.acc@gmail.com>
2023-07-12 15:25:41 +0800

    update: code doc

--- src/main/java/TaskContext.java
+++ src/main/java/TaskContext.java
@@ -2,7 +2,8 @@ import java.util.HashMap;
 
 /**
  * [FEATURE INFO]<br/>
- * context for a saggio task
+ * Context for a saggio task. <br/>
+ * The context does not provide any concurrent ensure, so this may need some user's work.
  *
  * @author Riicarus
  * @create 2023-6-8 19:17
